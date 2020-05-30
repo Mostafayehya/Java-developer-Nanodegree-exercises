f2a0d3572f14fe0e20b30e3ad68feab4c64d2de5
MostafaYahia <mostafayehyax23@gmail.com>
2020-05-30 23:03:07 +0200

    Fix uploading error
    Add Thymeleaf dependency however it didn't exist in the instructions, removing it from the POM causes the app to crash.

--- spring-boot-file-upload/src/main/java/com/example/springbootfileupload/controller/UploadController.java
+++ spring-boot-file-upload/src/main/java/com/example/springbootfileupload/controller/UploadController.java
@@ -16,16 +16,16 @@ public class UploadController {
     Map<String, Object> result = new HashMap<>();
     /// Receive message
     @RequestMapping("/uploadFile")
-    public Map<String, Object> upload(@RequestParam("attach") MultipartFile file) throws IOException {
+    public String upload(@RequestParam("attach") MultipartFile file) throws IOException {
         // File info
         System.out.println("File name = "  + file.getOriginalFilename());
         System.out.println("File type = " + file.getContentType());
 
         // Save to disk
         // file path example 1) Windows c:/, 3) Mac ~/Documents/
-        String filePath = "c:/";
+        String filePath = "C:/Users/moust/Documents/";
         file.transferTo(new File(filePath + file.getOriginalFilename()));
         result.put("Success", true);
-        return result;
+        return "success";
     }
 }
