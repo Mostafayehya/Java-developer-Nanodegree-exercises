cd7492c1a07d55811b3eeeb7ae0b540544b74cf3
MostafaYahia <mostafayehyax23@gmail.com>
2020-05-30 23:57:41 +0200

    Add thymeleaf project

---
+++ spring-boot-thymeleaf/src/main/java/com/example/springbootthymeleaf/controllers/UserController.java
@@ -0,0 +1,16 @@
+package com.example.springbootthymeleaf.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+
+    @RequestMapping("demo")
+    public String demo(Model model){
+        model.addAttribute("message","Hello, World");
+
+        return "demo";
+    }
+}
