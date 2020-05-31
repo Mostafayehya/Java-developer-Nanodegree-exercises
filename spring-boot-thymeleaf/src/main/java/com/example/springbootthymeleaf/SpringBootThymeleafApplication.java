36bffaf9ae76d81391c572dd2c93e0fc4aa44c19
MostafaYahia <mostafayehyax23@gmail.com>
2020-05-31 18:37:23 +0200

    Add thymeleaf project files

---
+++ spring-boot-thymeleaf/src/main/java/com/example/springbootthymeleaf/SpringBootThymeleafApplication.java
@@ -0,0 +1,16 @@
+package com.example.springbootthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootThymeleafApplication {
+
+	// run project
+	// goto    localhost:8080/demo
+	// goto    localhost:8080/demo2
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootThymeleafApplication.class, args);
+	}
+
+}
