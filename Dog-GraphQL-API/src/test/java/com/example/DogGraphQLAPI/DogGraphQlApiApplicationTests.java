9703d8821896337adc0f2551fca071d0a5c61bb6
MostafaYahia <mostafayehyax23@gmail.com>
2020-06-10 09:47:02 +0200

    Add Dog-GraphQL-API project
    Add GraphQL dependency manually

---
+++ Dog-GraphQL-API/src/test/java/com/example/DogGraphQLAPI/DogGraphQlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DogGraphQLAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogGraphQlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
